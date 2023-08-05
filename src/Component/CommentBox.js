409e0aaaf1332b249f7bf4c3483bc4f089adec44
Ranjeet Singh <sranjeet434@gmail.com>
2023-08-05 20:15:14 +0530

    nested comment box in watch page

---
+++ src/Component/CommentBox.js
@@ -0,0 +1,175 @@
+
+
+const CommentData=[
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+        
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+        
+                ]
+            }
+        ]
+    },
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    {
+                        name:"Ranjeet Singh",
+                        comment:"Jai Mata Di",
+                        reply:[
+                            
+                        ]
+                    },
+                    {
+                        name:"Ranjeet Singh",
+                        comment:"Jai Mata Di",
+                        reply:[
+                            
+                        ]
+                    },
+                ]
+            },
+        ]
+    },  
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+        ]
+    },
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+
+        ]
+    },
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+        ]
+    },
+    {
+        name:"Ranjeet Singh",
+        comment:"Jai Mata Di",
+        reply:[
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+            {
+                name:"Ranjeet Singh",
+                comment:"Jai Mata Di",
+                reply:[
+                    
+                ]
+            },
+        ]
+    },
+];
+
+const Comment=({data})=>{
+  
+    return(
+        <div className="flex gap-8 m-4 hover:bg-zinc-800 hover:rounded-xl duration-100 p-2">
+            <div>
+                <img className="w-12 rounded-3xl" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"/>
+            </div>
+            <div>
+                <p className="font-semibold text-lg">@{data.name}</p>
+                <p>{data.comment}</p>
+            </div>
+            {/* <p>{data.reply}</p> */}
+        </div>
+    )
+}
+
+const CommentList=({comment})=>{
+    return comment.map((comments,index)=>(
+        <div key={index} >
+            <Comment data={comments}/>
+            <div className="ml-20  border-l-2 border-l-zinc-700 p-2">
+                <CommentList comment={comments.reply}/>
+            </div>
+        </div>
+    ))
+}
+
+const CommmentBox=()=>{
+    return(
+        <div className="w-[56rem] m-4 bg-zinc-900 p-4 rounded-xl ">
+            <p className="text-2xl">Comments :</p>
+            <CommentList comment={CommentData}/>
+        </div>
+    );
+}
+export default CommmentBox;
--- src/Component/Watch.js
+++ src/Component/Watch.js
@@ -1,18 +1,23 @@
 import { useSearchParams } from "react-router-dom";
+import CommentBox from "./CommentBox";
 
 const Watch=()=>{
     const [searchParams]=useSearchParams();
     return(
-        <div className="pt-24 ml-4">
-        <iframe 
-        width="900"
-        height="450" 
-        src={"https://www.youtube.com/embed/"+ searchParams.get("v")}
-        title="YouTube video player" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowFullScreen
-        ></iframe>
-    </div>
+
+        <div className="flex flex-col text-white bg-black">
+            <div className="pt-24 ml-4">
+                <iframe
+                width="900"
+                height="450" 
+                src={"https://www.youtube.com/embed/"+ searchParams.get("v")}
+                title="YouTube video player"  
+                allowfullscreen
+                ></iframe>
+            </div>
+            <CommentBox/>
+        </div>
+        
     );
 }
 export default Watch;
